2a22fe2a51e3e5d7e136b2f8433a391ea48ce494
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-05-19 00:31:42 +0600

    home-page-done

--- src/pages/ToDo/ToDo.js
+++ src/pages/ToDo/ToDo.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import CreateToDoList from './CreateToDoList';
 
 const ToDo = () => {
     return (
         <div>
-
-
+            <h2 className='text-primary text-4xl font-bold text-center py-5'>You have added the following todo lists</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Task</th>
+                            <th>Description</th>
+                            <th>Complete ?</th>
+                            <th>Delete ?</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td><button className='btn btn-primary text-white'>Completed</button></td>
+                            <td><button className='btn btn-primary text-white'>Delete</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
