f1b433f7c28e41b13e36b212b485b1726d9ee9f6
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-05-19 01:54:54 +0600

    data-post

--- src/pages/ToDo/CreateToDoList.js
+++ src/pages/ToDo/CreateToDoList.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
 const CreateToDoList = () => {
+    const handleAddLists = (event) => {
+        event.preventDefault()
+        const title = event.target.title?.value
+        const description = event.target.description?.value
+        const email = event.target.email?.value
+        const task = { title, description, user: email }
+        console.log(task)
+        fetch(`http://localhost:5000/tasks`, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+        event.target.reset()
+    }
     return (
         <div class="card w-96 bg-base-100 shadow-xl mx-auto">
             <div class="card-body">
-                <form className='flex flex-col'>
+                <form onSubmit={handleAddLists} className='flex flex-col'>
                     <label htmlFor="Task Name" className='mb-2'>Task Name</label>
                     <input type="text" name="title" id="" class="input input-bordered w-50 max-w-xs" />
                     <label htmlFor="Task Name" className='mt-5 mb-2'>Description</label>
-                    <textarea type="text" name="title" id="" class="input input-bordered w-50 max-w-xs" />
+                    <textarea type="text" name="description" id="" class="input input-bordered w-50 max-w-xs" />
+                    <label htmlFor="Your Email " className='mt-5 mb-2'>Your Email</label>
+                    <input type="email" name="email" id="" class="input input-bordered w-50 max-w-xs" />
                     <input type="submit" className='btn btn-primary mt-3 w-48 text-white' value="Add task" />
                 </form>
 
